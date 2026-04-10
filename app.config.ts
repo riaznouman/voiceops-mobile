e86659b20e2f6d1b7d536280927ca8b332afba1f
Mehran Abbas <Mehran.abbas@cqumail.com>
2026-04-10 17:50:35 +0600

    add app.config.ts with voiceops config

---
+++ app.config.ts
@@ -0,0 +1,33 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "VoiceOps",
+  slug: "voiceops",
+  version: "1.0.0",
+  orientation: "portrait",
+  scheme: "voiceops",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.voiceops.mobile",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    package: "com.voiceops.mobile",
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  plugins: ["expo-router"],
+});
